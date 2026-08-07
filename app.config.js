51ad58b257d1abf39e283d3164a586ba10f63a15
SayMartin <martin@appfinningar.se>
2026-08-07 19:58:21 +0200

    Add logo SVG asset to the project

--- app.config.js
+++ app.config.js
@@ -5,15 +5,21 @@ module.exports = {
     version: '0.0.1',
     orientation: 'portrait',
     userInterfaceStyle: 'automatic',
+    icon: './assets/icon.png',
     ios: {
       bundleIdentifier: 'org.reactjs.native.example.WordseNative',
     },
     android: {
       package: 'com.wordsenative',
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#4f46e5',
+      },
     },
     web: {
       bundler: 'metro',
       output: 'single',
+      favicon: './assets/favicon.png',
     },
   },
 };
